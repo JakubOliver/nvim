a81083a403f7c2c87c5f074cb486f6590fad8ddb
Jakub Oliver Kubin <jakub.oliver.kubin@gmail.com>
2025-09-01 22:44:48 +0200

    Enable LSP

--- init.lua
+++ init.lua
@@ -673,8 +673,9 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        pyright = {},
+        csharp_ls = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
